9b79333d3a072bc02974939fcbb26615c617db15
kazungu <kazungu61@gmail.com>
2020-05-01 18:57:51 +0300

    save data using forms part 1

--- src/DjangoPractice/urls.py
+++ src/DjangoPractice/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from pages.views import home_view
-from products.views import product_detail_view
+from products.views import product_detail_view, product_create_view
 
 urlpatterns = [
 	path('', home_view, name='home'),
+	path('create/',product_create_view),
 	path('product/',product_detail_view),
     path('admin/', admin.site.urls),
 ]
